3af7ec6512481a8488d1d5d046e91752c502cdac
KameswaranS <KameswaranS@hexaware.com>
2015-06-16 08:42:06 +0530

    deleting

--- chart-app/charts/services/student-services.js
+++
@@ -1,5 +0,0 @@
-﻿var studentFactory = angular.module('studentFactory', ['ngResource']);
-
-studentFactory.factory('Profiles', ['$resource', 'appConsts', function ($resource, appConsts) {
-    return $resource(appConsts.jsonurl);
-}]);
